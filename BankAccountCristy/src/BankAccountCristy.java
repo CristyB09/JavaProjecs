67205e387d2753d1a6d144e3cc0c7fbd5f59aa16
Cristian Bonte <next_to_forever@yahoo.com>
2020-10-11 22:07:53 +0100

    Update BankAccountCristy.java

--- BankAccountCristy/src/BankAccountCristy.java
+++ BankAccountCristy/src/BankAccountCristy.java
@@ -8,7 +8,7 @@ public class BankAccountCristy {
     public static void main(String[] args) {
      
         Scanner input = new Scanner(System.in);
-        BankAcc ba = new BankAcc (0, "Cristy", "Bonte", "20-23-45", "234567897", "07462078543", 0, true, 0);
+        BankAcc ba = new BankAcc (0, "George", "Smith", "20-23-45", "234567897", "07673563478", 0, true, 0);
         System.out.println(ba);
         
         
